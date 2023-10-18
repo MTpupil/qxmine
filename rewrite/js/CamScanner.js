0d6dcafdec993c80d41c96c9260619931f5ea94d
mtpupil <970632312@qq.com>
2023-10-18 19:21:18 +0800

    update

--- rewrite/js/CamScanner.js
+++ rewrite/js/CamScanner.js
@@ -2,10 +2,6 @@
 CamScanner 解锁部分高级特权
 
 ***************************
-Quantumult X:
-
-[rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
 
 [mitm]
 hostname = ap*.intsig.net
