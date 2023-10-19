52e5e520cfad9e23bfb77f93689e13a6502d80fc
杨超越 <62123934+MTpupil@users.noreply.github.com>
2023-10-19 12:42:00 +0800

    更新 ysxz.js

--- rewrite/js/ysxz.js
+++ rewrite/js/ysxz.js
@@ -13,6 +13,8 @@ const user=/^https?:\/\/www\.yunshangxiezuo\.com\/api\/getAuthenticate_2/;
 
 if(user.test($request.url)){
   let obj=JSON.parse($response.body);
+  obj.data.user["name"] = "木瞳科技Pro";
+  obj.data.user["email"] = "破解成功，解锁所有功能";
   obj.data.user["vip"] = 1;
   obj.data.user["vip_forever"] = 1;
   obj.data.user["vip_last"] = "2099-12-13 00:00:00";
