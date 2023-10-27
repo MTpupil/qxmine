97ff72c5560bd2f9f33911806b9997c552cc2714
杨超越 <62123934+MTpupil@users.noreply.github.com>
2023-10-27 10:28:11 +0800

    更新 kuaidianyuedu.js

---
+++ rewrite/js/kuaidianyuedu.js
@@ -0,0 +1,12 @@
+const userUrl = /^https?:\/\/api\.crucio\.hecdn\.com\/v12\/user/;
+
+if(userUrl.test($request.url)){
+    const obj = JSON.parse($response.body);
+    const user = obj.data.users[0];
+    user.is_svip = true;
+    user.name = "木瞳科技"
+    
+    
+    let body = JSON.stringify(obj);
+    $done({body})
+}
