eb9e6fc97f9276f8b9c040f7c1bcf8637af05eca
杨超越 <62123934+MTpupil@users.noreply.github.com>
2023-10-31 12:54:19 +0800

    更新 notability.js

--- rewrite/js/notability.js
+++ rewrite/js/notability.js
@@ -9,4 +9,36 @@
  * 
  */
 
-let data = 
+const SCRIPT_NAME = 'Notability';
+
+let data = {
+    processAppleReceipt: {
+      error: 0,
+      subscription: {
+        productId: "com.gingerlabs.Notability.premium_subscription",
+        originalTransactionId: "570001184068302",
+        tier: "premium",
+        refundedDate: null,
+        refundedReason: null,
+        isInBillingRetryPeriod: false,
+        expirationDate: "2999-12-31T23:59:59.000Z",
+        gracePeriodExpiresAt: null,
+        overDeviceLimit: false,
+        expirationIntent: null,
+        __typename: "AppStoreSubscription",
+        user: null,
+        status: "canceled",
+        originalPurchaseDate: "2021-01-01T00:00:00.000Z"
+      },
+      __typename: "SubscriptionResult"
+    }
+};
+
+const plus = /^https?:\/\/notability\.com\/(global|subscriptions)/;
+
+if(plus.test($request.url)) {
+    let obj = JSON.parse($resopnse.body);
+    obj.data = data;
+    let body=JSON.stringify(obj);
+}
+$done()
