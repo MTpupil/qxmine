6819838ea02acbf46fdd8e99fb62ed7cddd837ba
杨超越 <62123934+MTpupil@users.noreply.github.com>
2024-09-08 10:37:44 +0800

    更新 duitang.js

--- rewrite/js/duitang.js
+++ rewrite/js/duitang.js
@@ -9,25 +9,24 @@
 
 const SCRIPT_NAME='堆糖';
 
-const user=/https?:\/\/(www|api)\.duitang\.com\/napi\/people\/me.*/;
-
-
+const duitang=/https?:\/\/(www|api)\.duitang\.com\/napi\/people\/me.*/;
+const naiyou=/ /;
 
 if(user.test($request.url)){
   let obj=JSON.parse($response.body);
   obj.data["username"]="木瞳科技Pro";
   obj.data["vip"]=true;
-obj.data["vip_level"]=11;
-obj.data["vip_end_at"]=4102380638;
-obj.data["vip_end_at_mills"]=4102380638000;
-obj.data["svip_mechanism"]=1;
-obj.data["latest_vip_level"]=11;
-obj.data["avatar"]="https://s1.ax1x.com/2022/11/23/z8LIPO.jpg";
-obj.data["vipstatus"]="open";
-obj.data["be_follow_count"]=999999;
-obj.data["ipaddr"]="木瞳科技Pro破解";
-obj.data["score"]=999999;
-obj.data["vip_remain"]=0;
+  obj.data["vip_level"]=11;
+  obj.data["vip_end_at"]=4102380638;
+  obj.data["vip_end_at_mills"]=4102380638000;
+  obj.data["svip_mechanism"]=1;
+  obj.data["latest_vip_level"]=11;
+  obj.data["avatar"]="https://s1.ax1x.com/2022/11/23/z8LIPO.jpg";
+  obj.data["vipstatus"]="open";
+  obj.data["be_follow_count"]=999999;
+  obj.data["ipaddr"]="木瞳科技Pro破解";
+  obj.data["score"]=999999;
+  obj.data["vip_remain"]=0;
   let body=JSON.stringify(obj);
   $done({body})
 }
