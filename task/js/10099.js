3ebde761c379d9341c3ab92b6391543f0abf2d57
杨超越 <62123934+MTpupil@users.noreply.github.com>
2024-11-01 01:47:59 +0800

    更新 10099.js

--- task/js/10099.js
+++ task/js/10099.js
@@ -5,7 +5,7 @@
 const $ = new Env("流量通知");
 const access = $.getdata("mtpupil_gdlltz_access");
 const updata = $.getdata("mtpupil_gdlltz_updata");
-const isMerge = $.getdata("mtpupil_gdlltz_isMerge"); // 用于控制是否合并
+const JSON.parse(isMerge = $.getdata("mtpupil_gdlltz_isMerge")); // 用于控制是否合并
 
 let gb = 1024 * 1024;
 let time = getFormattedDate();
